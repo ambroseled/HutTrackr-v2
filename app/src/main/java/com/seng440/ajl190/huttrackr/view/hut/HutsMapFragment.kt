c7f4408737749f0ed6546571be318bb8af57a9d4
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2020-05-08 11:55:36 +1200

    Working on displaying google map in fragment

--- app/src/main/java/com/seng440/ajl190/huttrackr/view/hut/HutsMapFragment.kt
+++ app/src/main/java/com/seng440/ajl190/huttrackr/view/hut/HutsMapFragment.kt
@@ -1,19 +1,19 @@
 package com.seng440.ajl190.huttrackr.view.hut
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.seng440.ajl190.huttrackr.R
 import com.seng440.ajl190.huttrackr.viewmodel.HutsMapViewModel
 
+
+
 class HutsMapFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HutsMapFragment()
-    }
 
     private lateinit var viewModel: HutsMapViewModel
 
@@ -21,13 +21,15 @@ class HutsMapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.huts_map_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(HutsMapViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(HutsMapViewModel::class.java)
     }
+
+
 
 }
