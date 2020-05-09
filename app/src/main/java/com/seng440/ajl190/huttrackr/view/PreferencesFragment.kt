e35d712e64833b4a25d320488bae252be33d02f1
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2020-05-09 20:47:15 +1200

    Added boilerplate preferences screen

--- app/src/main/java/com/seng440/ajl190/huttrackr/view/PreferencesFragment.kt
+++ app/src/main/java/com/seng440/ajl190/huttrackr/view/PreferencesFragment.kt
@@ -1,15 +1,12 @@
 package com.seng440.ajl190.huttrackr.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceFragmentCompat
 import com.seng440.ajl190.huttrackr.R
 import com.seng440.ajl190.huttrackr.viewmodel.PreferencesViewModel
 
-class PreferencesFragment : Fragment() {
+class PreferencesFragment : PreferenceFragmentCompat() {
 
     companion object {
         fun newInstance() = PreferencesFragment()
@@ -17,17 +14,21 @@ class PreferencesFragment : Fragment() {
 
     private lateinit var viewModel: PreferencesViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.preferences_fragment, container, false)
-    }
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.preferences_fragment, container, false)
+//    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(PreferencesViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(PreferencesViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
 }
--- app/src/main/java/com/seng440/ajl190/huttrackr/viewmodel/HutsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.seng440.ajl190.huttrackr.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class HutsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
