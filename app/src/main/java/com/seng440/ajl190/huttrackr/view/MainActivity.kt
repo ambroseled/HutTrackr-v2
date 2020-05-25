664daf1980e663771a817131de5d35e45f08d3de
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2020-05-25 15:19:12 +1200

    Implemented read temperature functionality

--- app/src/main/java/com/seng440/ajl190/huttrackr/view/MainActivity.kt
+++ app/src/main/java/com/seng440/ajl190/huttrackr/view/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.seng440.ajl190.huttrackr.view
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
@@ -12,15 +20,19 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.seng440.ajl190.huttrackr.R
-import kotlinx.android.synthetic.main.activity_huts.*
+import kotlinx.android.synthetic.main.action_bar.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var navController: NavController
+    private lateinit var sensorManager: SensorManager
+    private var temperature: Float = 0.toFloat()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_huts)
+        setContentView(R.layout.activity_main)
 
         navController = Navigation.findNavController(this, R.id.hutsListFragment)
 
@@ -30,8 +42,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(actionBar)
 
         NavigationUI.setupActionBarWithNavController(this, navController)
-
-
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
 
@@ -50,5 +61,39 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.hutsListFragment)
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    private fun loadTemp() {
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+
+        if (sensor != null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        } else {
+            tempActionBar.visibility = View.GONE
+            Toast.makeText(this, "No temperature sensor present on device", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            if (event.values.isNotEmpty()) {
+                temperature = event.values[0]
+                tempActionBar.text = "$temperature°"
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadTemp()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
