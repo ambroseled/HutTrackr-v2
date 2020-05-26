18749104279d7c7a603ce5a31d4ba51602f60e82
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2020-05-26 14:06:13 +1200

    moved toen functionality to new wish list add location

--- app/src/main/java/com/seng440/ajl190/huttrackr/view/track/TrackFragment.kt
+++ app/src/main/java/com/seng440/ajl190/huttrackr/view/track/TrackFragment.kt
@@ -4,6 +4,8 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -100,12 +102,18 @@ class TrackFragment : ScopedFragment(), KodeinAware {
             addVisit()
             closeFabMenu()
             Toast.makeText(requireContext(), "Visit added", Toast.LENGTH_LONG).show()
+            val tone = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+            tone.startTone(ToneGenerator.TONE_PROP_BEEP)
+            tone.release()
         }
 
         wishFab.setOnClickListener {
             addHutToWishList()
             closeFabMenu()
             Toast.makeText(requireContext(), "Added to wish list", Toast.LENGTH_LONG).show()
+            val tone = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+            tone.startTone(ToneGenerator.TONE_PROP_BEEP)
+            tone.release()
         }
 
         smsFab.setOnClickListener {
